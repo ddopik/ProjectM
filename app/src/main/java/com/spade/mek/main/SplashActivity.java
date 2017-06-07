301c9bac2ebee791c1006f9de97cc42cc9dda930
Ayman Abouzaid <ayman.abouzaid@spade.studio>
2017-06-07 15:23:56 +0200

    AZ - add social login library

---
+++ app/src/main/java/com/spade/mek/main/SplashActivity.java
@@ -0,0 +1,10 @@
+package com.spade.mek.main;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by spade on 6/7/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+}
